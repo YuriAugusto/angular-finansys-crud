ca79c8401c1a00555fbee0022b9c47057e3ff192
YuriAugusto <komesu.yuri.a@gmail.com>
2022-02-03 08:22:39 -0300

    adicao do in-memory-database para categorias

---
+++ src/app/pages/categories/in-memory-database.ts
@@ -0,0 +1,17 @@
+import { InMemoryDbService } from "angular-in-memory-web-api";
+
+export class InMemoryDAtaBase implements InMemoryDbService{
+
+  createDb(){
+    const categories = [
+      { id: 1, name: 'Moradia', description: 'Pagar contas de casa' },
+      { id: 2, name: 'Saúde', description: 'Pagar plano de saúde' },
+      { id: 3, name: 'Lazer', description: 'Cinema, parque e etc' },
+      { id: 4, name: 'Salário', description: 'Recebimento de salário fixo' },
+      { id: 5, name: 'Freelas', description: 'Recebimento de salário variado' }
+    ];
+
+    return { categories }
+  }
+
+}
