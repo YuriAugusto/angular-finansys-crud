f50de3ad30e73d40cd40aab5c8bb7ee82c9ff314
YuriAugusto <komesu.yuri.a@gmail.com>
2022-02-08 09:32:17 -0300

    melhoria no category-form.component

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -2,12 +2,12 @@ import { AfterContentChecked, Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 
-import { switchMap } from "rxjs/operators";
+import { switchMap } from 'rxjs/operators';
+import toastr from 'toastr';
 
 import { Category } from '../shared/category.model';
 import { CategoryService } from '../shared/category.service';
 
-import toastr from "toastr";
 
 @Component({
   selector: 'app-category-form',
@@ -40,9 +40,18 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
     this.setPageTitle();
   }
 
+  submitForm(){
+    this.submittingForm = true;
+
+    if(this.currentAction == 'new')
+      this.createCategory();
+    else
+      this.updateCategory();
+  }
+
   private setCurrentAction() {
-    //retorna um array da rota segmentada pelas "/" a partir de categories ex: categories/new url[0]==new
-    if(this.route.snapshot.url[0].path == "new")
+    //retorna um array da rota segmentada pelas '/' a partir de categories ex: categories/new url[0]==new
+    if(this.route.snapshot.url[0].path == 'new')
       this.currentAction = 'new';
     else
       this.currentAction = 'edit'
@@ -60,7 +69,7 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
     if(this.currentAction == 'edit'){
 
       this.route.paramMap.pipe(
-        switchMap(params => this.categoryService.getByIdd(+params.get("id")))
+        switchMap(params => this.categoryService.getByIdd(+params.get('id')))
       )
       .subscribe(
         (category) => {
@@ -79,6 +88,50 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
       const categoryName = this.category.name || '...';//this.category.name se for null uma string vazia é atribuída
       this.pageTitle = 'Editando Categoria: ' + categoryName;
     }
+  }
+
+  private createCategory(){
+    //abaixo os valores preenchidos no form são recuperados, há tipagem dos dados e por fim atribuição
+    const category: Category = Object.assign(new Category(), this.categoryForm.value);
+
+    this.categoryService.create(category)
+    .subscribe(
+      category => this.actionsForSuccess(category),
+      error => this.actionsForError(error)
+    )
+  }
+
+  private updateCategory(){
+    const category: Category = Object.assign(new Category(), this.categoryForm.value);
+
+    this.categoryService.update(category)
+    .subscribe(
+      category => this.actionsForSuccess(category),
+      error => this.actionsForError(error)
+    )
+  }
+
+  private actionsForSuccess(category: Category){
+    toastr.success('Solicitação processada com sucesso!');
+
+    // esse método redireciona o usuário para '/categories' depois redireciona novamente para '/categories/:id/edit'
+    // o objetivo disso é fazer o componente ser recarregado pois no onInit existem diversas functions essenciais
+    // para o funcionamento do componente
+    // skipLocationChange: true faz com que o usuário não consiga voltar pra determinada página
+    this.router.navigateByUrl('categories', {skipLocationChange: true}).then(
+      () => this.router.navigate(['categories', category.id, 'edit'])
+    )
+  }
+
+  private actionsForError(error){
+    toastr.error('Ocorreu um erro ao processar a sua solicitação!');
+
+    this.submittingForm = false;
+
+    if(error.status === 422)
+      this.serverErrormessages = JSON.parse(error._body).erros;//retorna um array de erros
+    else
+      this.serverErrormessages = ["Falha na comunicação com o servidor. Por favor tente mais tarde."]
   }
 
 }
