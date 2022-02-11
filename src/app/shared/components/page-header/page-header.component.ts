3a2d5d3e57c7b2584a3e413e2d27b470b4bbf98f
YuriAugusto <komesu.yuri.a@gmail.com>
2022-02-11 15:38:24 -0300

    adicao de page-header.component

---
+++ src/app/shared/components/page-header/page-header.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-header',
+  templateUrl: './page-header.component.html',
+  styleUrls: ['./page-header.component.css']
+})
+export class PageHeaderComponent implements OnInit {
+
+  // quando houver passagem de valor para as propriedades o bind deve
+  // ser feito através de 'page-title', 'button-class', 'button-text' etc...
+  @Input('page-title') pageTitle: string;
+  @Input('button-class') buttonClass: string;
+  @Input('button-text') buttonText: string;
+  @Input('button-link') buttonLink: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,12 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
+import { PageHeaderComponent } from './components/page-header/page-header.component';
 
 @NgModule({
   declarations: [
-    BreadCrumbComponent
+    BreadCrumbComponent,
+    PageHeaderComponent
   ],
   imports: [
     CommonModule,
@@ -20,7 +22,8 @@ import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.compon
     ReactiveFormsModule,
     RouterModule,
     //componentes compartilhados
-    BreadCrumbComponent
+    BreadCrumbComponent,
+    PageHeaderComponent
   ]
 })
 export class SharedModule { }
