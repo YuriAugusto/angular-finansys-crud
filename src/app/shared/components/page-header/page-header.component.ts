590620379db717ea8133b199babba2d240d50da4
YuriAugusto <komesu.yuri.a@gmail.com>
2022-02-14 12:36:38 -0300

    melhoria no page-header.component

--- src/app/shared/components/page-header/page-header.component.ts
+++ src/app/shared/components/page-header/page-header.component.ts
@@ -10,6 +10,7 @@ export class PageHeaderComponent implements OnInit {
   // quando houver passagem de valor para as propriedades o bind deve
   // ser feito através de 'page-title', 'button-class', 'button-text' etc...
   @Input('page-title') pageTitle: string;
+  @Input('show-button') showButton: boolean = true;//definição de valor default
   @Input('button-class') buttonClass: string;
   @Input('button-text') buttonText: string;
   @Input('button-link') buttonLink: string;
